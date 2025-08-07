4fb61816f46709b76b59da963aed76ec081fbcf1
viperzin <zetsuuki@gmail.com>
2025-08-06 23:10:41 -0300

    Usamos un array y una funcion para agregar y contar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Creamos un array que compile los amigos
+let amigos = [];
+
+function agregarAmigo() {
+  let input = document.getElementById('amigo');
+   if (input.value.length < 1) {
+    alert('Porfavor, inserte un nombre');
+    return;
+  }
+  // Agregamos los amigos introducidos al array
+  amigos.push(input.value);
+  mostrarAmigos();
+  document.getElementById('resultado').innerHTML = 'Amigo agregado' + input.value;
+  input.value = '';
+
+  console.log(amigos);
+}
